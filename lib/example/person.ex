7b86a3ecde76f26202f3b37e2d7994d5271e225b
kamalogudah <mcpaul2058@gmail.com>
2019-02-09 01:33:01 +0300

    Adding Changes programatically

--- lib/example/person.ex
+++ lib/example/person.ex
@@ -16,6 +16,18 @@ defmodule Example.Person do
     |> validate_fictional_name()
   end
 
+  def sign_up(params) do
+    %Example.Person{}
+    |> Example.Person.registration_changeset(params)
+    |> Repo.insert()
+  end
+
+  def registration_changeset(struct, params) do
+    struct
+    |> cast(params, [:name])
+    |> set_name_if_anonymous()
+  end
+
   @fictional_names ["Black Panther", "Wonder Woman", "Spiderman"]
   def validate_fictional_name(changeset) do
     name = get_field(changeset, :name)
@@ -24,6 +36,16 @@ defmodule Example.Person do
       changeset
     else
       add_error(changeset, :name, "is not a superhero")
+    end
+  end
+
+  def set_name_if_anonymous(changeset) do
+    name = get_field(changeset, :name)
+
+    if is_nil(name) do
+      put_change(changeset, :name, "Anonymous")
+    else
+      changeset
     end
   end
 end

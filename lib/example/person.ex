4182f5d85e042f7beaafaa599d52462957b28c6f
kamalogudah <mcpaul2058@gmail.com>
2019-02-09 01:18:30 +0300

    Custom Validations

--- lib/example/person.ex
+++ lib/example/person.ex
@@ -13,5 +13,17 @@ defmodule Example.Person do
     |> validate_required([:name, :age])
     |> validate_length(:name, min: 2)
     |> validate_inclusion(:age, 1..130)
+    |> validate_fictional_name()
+  end
+
+  @fictional_names ["Black Panther", "Wonder Woman", "Spiderman"]
+  def validate_fictional_name(changeset) do
+    name = get_field(changeset, :name)
+
+    if name in @fictional_names do
+      changeset
+    else
+      add_error(changeset, :name, "is not a superhero")
+    end
   end
 end

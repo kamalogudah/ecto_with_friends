541a780dbaaae712a3747798621fc76c7b294de4
kamalogudah <mcpaul2058@gmail.com>
2019-02-09 01:15:15 +0300

    Validations

--- lib/example/person.ex
+++ lib/example/person.ex
@@ -1,8 +1,17 @@
 defmodule Example.Person do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "people" do
     field(:name, :string)
-    field(:age, :integer, default: 0)
+    field(:age, :integer, default: 1)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :age])
+    |> validate_required([:name, :age])
+    |> validate_length(:name, min: 2)
+    |> validate_inclusion(:age, 1..130)
   end
 end
